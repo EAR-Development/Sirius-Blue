ae8bededa4b9f0136dd9fd35999f229484673bc6
Alexander Engelsberger <theraedrummer@gmail.com>
2017-10-21 16:44:42 +0200

    Child Follows Mother

--- Assets/ChildController.cs
+++ Assets/ChildController.cs
@@ -10,22 +10,33 @@ public class ChildController : MonoBehaviour {
 	public GameObject mother;
 	public float speed = 5.0f;
 
+	float randomOffset;
+
 	// Use this for initialization
 	void Start () {
-		
+		randomOffset = Random.Range (-1, 1) * 0.1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (motherSeen) {
 			float movement = 0;
-			if (!directionIsLeft && transform.position.x < mother.transform.position.x) {
-				movement = speed;
+			float hspeed = 0;
+			if (!directionIsLeft && transform.position.x < (mother.transform.position.x + randomOffset)) {
+				movement = speed * (Mathf.Abs(transform.position.x - mother.transform.position.x )+1) * Time.deltaTime;
 			}
-			if (directionIsLeft && transform.position.x > mother.transform.position.x) {
-				movement = -speed;
+			if (directionIsLeft && transform.position.x > (mother.transform.position.x + randomOffset)) {
+				movement = -speed * (Mathf.Abs(transform.position.x - mother.transform.position.x)+1) * Time.deltaTime;
 			}
-			transform.position = new Vector3 (transform.position.x + movement, transform.position.y, transform.position.z);
+
+			if (Mathf.Abs(transform.position.x - (mother.transform.position.x + randomOffset)) < 0.1) {
+				Vector3 rot = transform.eulerAngles;
+				rot.z = 180;
+				transform.eulerAngles = rot;
+				hspeed = (mother.transform.position.y - transform.position.y) * speed * Time.deltaTime;
+			}
+
+			transform.position = new Vector3 (transform.position.x + movement, transform.position.y + hspeed, transform.position.z);
 		}
 	}
 }
