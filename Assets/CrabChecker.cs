d98c9119d38536892d6e352e272219124fd5338f
Alexander Engelsberger <theraedrummer@gmail.com>
2017-10-21 13:07:28 +0200

    Crab Part I

---
+++ Assets/CrabChecker.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabChecker : MonoBehaviour {
+
+	public bool isOverRubish;
+	public bool isOverGround;
+	public bool isOverShell;
+	public GameObject shell;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if (other.CompareTag ("rubish")) {
+			isOverRubish = true;
+		}
+		if (other.CompareTag ("ground")) {
+			isOverGround = true;
+		}
+		if (other.CompareTag ("shell")) {
+			isOverShell = true;
+			shell = other.gameObject; 
+		}
+	}
+		
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.CompareTag ("rubish")) {
+			isOverRubish = true;
+		}
+		if (other.CompareTag ("ground")) {
+			isOverGround = true;
+		}
+		if (other.CompareTag ("shell")) {
+			isOverShell = true;
+			shell = other.gameObject; 
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.CompareTag ("rubish")) {
+			isOverRubish = false;
+		}
+		if (other.CompareTag ("ground")) {
+			isOverGround = false;
+		}
+		if (other.CompareTag ("shell")) {
+			isOverShell = false;
+			shell = null; 
+		}
+	}
+}
---
+++ Assets/CrabController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabController : MonoBehaviour {
+
+	public GameObject leftFreeCheck;
+	public GameObject rightFreeCheck;
+
+	public bool directionIsLeft = true;
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movement = 0;
+
+		if (!directionIsLeft && ! rightFreeCheck.GetComponent<CrabChecker>().isOverRubish) {
+			movement = speed;
+		}
+		if (directionIsLeft && ! leftFreeCheck.GetComponent<CrabChecker>().isOverRubish) {
+			movement = -speed;
+		}
+
+
+		if (!directionIsLeft && rightFreeCheck.GetComponent<CrabChecker>().isOverShell) {
+			GameObject.Destroy (rightFreeCheck.GetComponent<CrabChecker>().shell);
+			GameObject.Destroy (this.gameObject);
+		}
+		if (directionIsLeft && leftFreeCheck.GetComponent<CrabChecker>().isOverShell) {
+			GameObject.Destroy (leftFreeCheck.GetComponent<CrabChecker>().shell);
+			GameObject.Destroy (this.gameObject);
+		}
+
+
+		transform.position = new Vector3 (transform.position.x + movement, transform.position.y, transform.position.z);
+	}
+}
