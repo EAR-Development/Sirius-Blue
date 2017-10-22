df206b849f43cd0cebfb310914fba7ccbbc262d8
Alexander Engelsberger <theraedrummer@gmail.com>
2017-10-22 16:22:25 +0200

    Menu fertig

--- Assets/scripts/MenuController.cs
+++ Assets/scripts/MenuController.cs
@@ -5,11 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+	public GameObject mainMenu;
+	public GameObject impressum;
+
 	public void startGame(){
 		SceneManager.LoadScene("main");
 	}
 
 	public void showImpressum(){
+		mainMenu.SetActive (false);
+		impressum.SetActive (true);
+	}
+
+	public void hideImpressum(){
+		mainMenu.SetActive (true);
+		impressum.SetActive (false);
 	}
 
 	public void exitGame(){
